79d551ae64859cb8909f9be1669f1287bb9b6142
韩超 <hanchao@udesk.cn>
2023-12-12 00:25:28 +0800

    推送代码触发自动构建

--- config/config.prod.js
+++ config/config.prod.js
@@ -20,9 +20,9 @@ exports.sequelize = {
     underscored: false,
     charset: 'utf8',
     timestamp: true,
-    createdAt: 'created_at',
-    updatedAt: 'updated_at',
-    deletedAt: 'deleted_at',
+    // createdAt: 'created_at',
+    // updatedAt: 'updated_at',
+    // deletedAt: 'deleted_at',
   },
   dialectOptions: {
     dateStrings: true,
