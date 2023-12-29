08459768fb32b011763695849a6ac6ee26c51e90
韩超 <hanchao@udesk.cn>
2023-12-29 20:25:09 +0800

    只保留最新的3个备份文件

--- app/schedule/backup.js
+++ app/schedule/backup.js
@@ -1,5 +1,7 @@
 'use strict';
 const child_process = require('child_process');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = {
     schedule: {
@@ -19,12 +21,35 @@ module.exports = {
         // -p${process.env.MySqlPassword} 之间不能有空格,否则会打断自动进程,需要手动输入密码
         const dumpCommand = `mysqldump -h ${process.env.MySqlHost} -P ${process.env.MySqlPort} -u ${process.env.MySqlUserName} -p${process.env.MySqlPassword} ${process.env.MySqlDatabase} > ./backup/${timestamp}_backup.sql`;
 
+        const { backup_dir } = ctx.app.config.static;
+
+        const grandparentDir = path.dirname(path.dirname(__dirname));
+
+        const backupDir = path.join(grandparentDir, backup_dir);
+
+        console.log('backupDir------', backupDir);
+
+        // 获取备份目录中的所有文件
+        const files = fs.readdirSync(backupDir);
+
+        // 过滤出所有以 _backup.sql 结尾的文件
+        const backupFiles = files.filter(file => file.endsWith('_backup.sql'));
+
+        // 根据文件名对文件进行排序
+        backupFiles.sort((a, b) => b.localeCompare(a));
+
+        // 获取除了最新的三个文件之外的所有文件
+        const filesToDelete = backupFiles.slice(3);
+
+        // 删除这些文件
+        filesToDelete.forEach(file => fs.unlinkSync(path.join(backupDir, file)));
+
         child_process.exec(dumpCommand, (error, stdout, stderr) => {
             if (error) {
                 ctx.logger.error(`Error executing ${dumpCommand}: ${error}`);
                 return;
             }
-            console.log('备份成功');
+            console.log('备份成功', config);
             ctx.logger.info(`Database backed up successfully.`);
         });
     },
